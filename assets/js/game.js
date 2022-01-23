7fc4b3cd6e8c2faaa3e39de23981635bd3fe726f
Leigh Pfeiffer <leigh.pfeiffer@gmail.com>
2022-01-22 21:47:54 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -29,7 +29,7 @@ var fight = function(enemyName) {
     }
 
      // remove enemy's health by subtracting the amount set in the playerAttack variable
-     enemyHealth = enemyHealth - playerAttack;
+     enemyHealth = Math.max(0, enemyHealth - playerAttack);
      console.log(
        playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
      );
@@ -39,7 +39,7 @@ var fight = function(enemyName) {
        window.alert(enemyName + ' has died!');
  
        // award player money for winning
-       playerMoney = playerMoney + 20;
+       playerMoney = Math.max(0, playerMoney - 10);
  
        // leave while() loop since enemy is dead
        break;
@@ -48,7 +48,7 @@ var fight = function(enemyName) {
      }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    playerHealth = Math.max(0, playerHealth - enemyAttack);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -83,7 +83,8 @@ var startGame = function() {
       var pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber();
+
 
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
@@ -109,6 +110,23 @@ var startGame = function() {
   // after loop ends, we are either out of playerHealth or enemies to fight, so run the endGame function
   endGame();
 };
+
+// function to generate a random numeric value
+var randomNumber = function(40, 60) {
+  var value = Math.floor(Math.random() * (21)) + 40;
+
+  return value;
+};
+
+// generate random damage value based on player's attack power
+var damage = randomNumber(playerAttack -3, playerAttack);
+
+enemyHealth = Math.max(0, enemyHealth - damage);
+
+var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+playerHealth = Math.max(0, playerHealth - damage);
+
 
 // function to end the entire game
 var endGame = function() {
